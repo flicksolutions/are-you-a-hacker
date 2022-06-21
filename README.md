e78919b452de453619c621af579695379b19a727
flicksolutions <sebastian@flick.solutions>
2022-06-21 21:30:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ob3vzm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ob3vzm)
---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+let val = null;
+const targetVal = 38;
+let answerDiv, wrongDiv
+const handleClick = () => {
+  answerDiv.style.display = "none";
+  wrongDiv.style.display = "none";
+
+  if (val === targetVal) {
+    answerDiv.style.display = "block";
+  } else {
+    wrongDiv.style.display = "block";
+  }
+}
+/*$:{
+  if (val === targetVal) {
+      answerDiv.style.display = "block";
+      wrongDiv.style.display = "none";
+    }
+}*/
+</script>
+
+<main>
+  <label for="q">An welche Zahl denke ich gerade?</label><input type="number" id="q" bind:value={val} /> <button on:click={handleClick}>Antworten</button>
+
+  <div class="answer" bind:this={answerDiv}>
+    <b>Das ist richtig! Ich denke an {targetVal}!</b>
+  </div>
+  <div class="wrong" bind:this={wrongDiv}>
+    <p>leider nein, daran denke ich nicht.</p>
+  </div>
+</main>
+
+<style>
+.wrong, .answer {
+  display: none;
+}
+
+</style>
